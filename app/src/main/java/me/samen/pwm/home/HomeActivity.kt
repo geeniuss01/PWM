d2b4b81705941dde024e70e7e0accccf8a75e379
Dhanyamraju Santosh Kumar <geeniuss01@gmail.com>
2017-05-15 19:11:14 +0530

    used lazy init, immutable types

--- app/src/main/java/me/samen/pwm/home/HomeActivity.kt
+++ app/src/main/java/me/samen/pwm/home/HomeActivity.kt
@@ -2,13 +2,12 @@ package me.samen.pwm.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import android.view.View
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.content_home.*
 import me.samen.pwm.R
 import me.samen.pwm.common.Data
 import me.samen.pwm.common.PWMApp
@@ -18,52 +17,44 @@ import me.samen.pwm.setup.AppPreferenceManager
 import me.samen.pwm.setup.SetupActivity
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
-    var fab: FloatingActionButton? = null
-    var recyclerView: RecyclerView? = null
-    var appData: Data? = null
-    var adapter: AccListAdapter? = null
-
+    val appData : Data by lazy { (application as PWMApp).appData }
+    val listAdapter: AccListAdapter by lazy { AccListAdapter(appData.getSugarAcc(), this, this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val toolBar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolBar)
-
-        appData = (application as PWMApp).appData
-
-        fab = (findViewById(R.id.fab) as FloatingActionButton?)!!
-        fab?.setOnClickListener { view ->
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener {
             startActivity(Intent(this, EditActivity::class.java))
         }
-        adapter = AccListAdapter(appData?.getSugarAcc()!!, this, this)
-        recyclerView = (findViewById(R.id.list) as RecyclerView?)!!
-        recyclerView?.adapter = adapter
-        recyclerView?.layoutManager = LinearLayoutManager(this)
+        list.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
     override fun onResume() {
         super.onResume()
         if (AppPreferenceManager.getPreference(this).isFirstTimeLaunch) {
             startActivity(Intent(this, AppIntroActivity::class.java))
-        } else if (!appData?.authenticated!!) {
+        } else if (!appData.authenticated) {
             startActivity(Intent(this, SetupActivity::class.java))
         } else {
-            appData?.getSugarAcc()
-            adapter?.notifyDataSetChanged()
+            appData.getSugarAcc()
+            listAdapter.notifyDataSetChanged()
         }
     }
 
     override fun onPause() {
         super.onPause()
-        appData?.authenticated = false
+        appData.authenticated = false
     }
 
     override fun onClick(v: View?) {
         val pos = v?.tag as Int
-        val pwd = appData?.getSugarAcc()!!.get(pos).pwd as String
-        when (v?.id) {
-            R.id.textView, R.id.textView2 -> Snackbar.make(v!!, pwd, Snackbar.LENGTH_SHORT).setAction("ach", null).show()
+        val pwd = appData.getSugarAcc().get(pos).pwd as String
+        when (v.id) {
+            R.id.textView, R.id.textView2 -> Snackbar.make(v, pwd, Snackbar.LENGTH_SHORT).setAction("ach", null).show()
             R.id.textView3 -> {
                 val intnt = Intent(this, EditActivity::class.java)
                 intnt.putExtra("pos", pos)
